01ee8e1385f29b2d0d056567c7068839b39a5a83
lokjunneo <neolokjun@gmail.com>
2024-07-21 07:35:06 +0800

    Agent available unrelated to scripts, but because another task was stuck running

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 set -x
-docker kill my-apache-php-app
-docker rm my-apache-php-app
-#docker container stop my-apache-php-app
+# docker kill my-apache-php-app
+# docker rm my-apache-php-app
+docker container stop my-apache-php-app
