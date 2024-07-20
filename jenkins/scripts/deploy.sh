b5691dae23fe320201b545d9afa183adbed492a6
lokjunneo <raizutiamat@gmail.com>
2024-07-21 06:50:26 +0800

    Fixed bind mount in deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v https://github.com/lokjunneo/jenkins-php-selenium-test
+docker run -d -p 80:80 --name my-apache-php-app -v /Users/lokjun/Documents/SSD_Practical/jenkins-php-selenium-test:/var/www/html php:7.2-apache
 
 set +x
 
