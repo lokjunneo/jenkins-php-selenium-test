41ebb5a2eb2a160fc89a37cece3cc8aff3d34b9f
lokjunneo <raizutiamat@gmail.com>
2024-07-21 07:29:28 +0800

    Removed --rm and reverted kill.sh in hopes to free up executor

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d --rm -p 80:80 --name my-apache-php-app -v /Users/lokjun/Documents/SSD_Practical/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
-sleep 3
+docker run -d -p 80:80 --name my-apache-php-app -v /Users/lokjun/Documents/SSD_Practical/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+sleep 1
 set +x
 
 echo 'Now...'
